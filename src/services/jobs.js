39876d0ac43c6284aa5196d24642f61f4324e1ba
Daniel Alvarez <35469266+Dansata@users.noreply.github.com>
2021-05-04 18:49:48 -0500

    Feature/services (#4)
    * Setup parallel servers for api and react app
    
    * Adjust RecentJobs component image
    
    * Create services and environment url

---
+++ src/services/jobs.js
@@ -0,0 +1,29 @@
+const API_URL_BASE = process.env.REACT_APP_API_URL || "";
+
+const getAllJobs = async () => {
+  try {
+    const resp = await fetch(`${API_URL_BASE}/api/jobs`);
+    const jobs = await resp.json();
+
+    return jobs;
+  } catch (error) {
+    throw Error("Ohhps");
+  }
+};
+
+const getJob = async (id) => {
+  try {
+    const resp = await fetch(`${API_URL_BASE}/api/jobs/${id}`);
+    const job = await resp.json();
+
+    return job;
+  } catch (error) {
+    throw Error("Ohhps");
+  }
+};
+
+const createJob = (job) => {
+  return job;
+};
+
+export { getAllJobs, getJob, createJob };
