e7b8beb5c589a1cb4abe086f4d3cc79faa7c2801
Daniel Alvarez <danielalvarezk@gmail.com>
2021-05-04 20:28:13 -0500

    WIP Apply job

--- src/components/ApplyForJob/index.js
+++ src/components/ApplyForJob/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
+import { getJob } from "../../services/jobs";
+import { useParams } from "react-router-dom";
 
-import Header from './Header';
-import Main from './Form';
+import Header from "./Header";
+import Main from "./Form";
 
-const ApplyJob = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
-);
+const ApplyJob = () => {
+  const { id } = useParams();
+  useEffect(() => {
+    const theJob = getJob(id);
+  }, []);
+  const [theJob, setJob] = useState(null);
+  setJob(id);
+
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
 
 export default ApplyJob;
