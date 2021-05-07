027d527e854691c576cc52cabccc105ff4471007
Daniel Alvarez <35469266+Dansata@users.noreply.github.com>
2021-05-06 19:46:09 -0500

    Feature/aplyjob (#5)
    * WIP Apply job
    
    * MAde Header component dynamic

--- src/components/ApplyForJob/index.js
+++ src/components/ApplyForJob/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
+import { getJob } from "../../services/jobs";
+import { useParams } from "react-router-dom";
 
-import Header from './Header';
-import Main from './Form';
+import Header from "./Header";
+import Main from "./Form";
 
-const ApplyJob = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
-);
+const ApplyJob = () => {
+  const [jobDetail, setJobDetail] = useState([]);
+  const { id } = useParams();
+  const getJobDetail = async () => {
+    const job = await getJob(id);
+    setJobDetail(job);
+  };
+  useEffect(() => {
+    getJobDetail();
+  }, [id]);
+
+  return (
+    <>
+      <Header job={jobDetail} />
+      <Main />
+    </>
+  );
+};
 
 export default ApplyJob;
